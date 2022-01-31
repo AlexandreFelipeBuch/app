f8c6d6c6ca04b28ece3a05b26cb5454b9c213698
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2022-01-31 17:38:13 -0300

    Add Screens

--- src/screens/Barber/styles.js
+++ src/screens/Barber/styles.js
@@ -2,5 +2,3 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView``;
-export const ButtonVoltar = styled.TouchableOpacity``;
-export const TextButton = styled.Text``;
--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ButtonVoltar, TextButton} from './styles';
 import {Container} from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 export default () => {
+  const navigation = useNavigation();
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignUp'}],
+    });
+  };
+
   return (
     <Container>
-      <Text>Favorites</Text>
+      <ButtonVoltar onPress={handleMessageButtonClick}>
+        <TextButton>Clique aqui</TextButton>
+      </ButtonVoltar>
     </Container>
   );
 };
