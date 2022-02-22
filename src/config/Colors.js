5712e0284c457bb5ec9f1e4790668e9abc768494
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2022-02-22 15:42:29 -0300

    config

---
+++ src/config/Colors.js
@@ -0,0 +1,56 @@
+export const Colors = {
+  primary: '#148c32',
+  secondary: '#70ff00',
+  primaryLight: '#fff',
+
+  // default
+  text: '#484848',
+  error: '#ec5e5a',
+  action: '#007171',
+  textDisabled: '#cac9cb',
+  blackDivide: 'rgba(0,0,0,0.05)',
+  blackTextSecondary: 'rgba(0,0,0,0.5)',
+  separator: '#eaeded',
+  background: '#ffffff',
+  graySubtitle: '#7a747f',
+  lightGray: '#a5a6a6',
+  textBorderColor: '#9b9b9b',
+  textBackgroundColor: '#f0f1f3',
+  textPlaceholder: '#dadadc',
+  textUnderLine: '#dedede',
+  loadingPrimary: '#e1e2e4',
+  loadingSecondary: '#f1f5fa',
+  sessionBackground: '#E5E5E5',
+
+  // ButtonGradient
+  gradientPrimary: 'rgb(255, 152, 32)',
+  gradientSecundary: 'rgb(223, 133, 29)',
+  gradientShadom: 'rgba(223, 152, 29, 0.33)',
+  gradientForeground: '#ffffff',
+  gradientPrimaryDisabled: 'rgba(255, 152, 32, 0.5)',
+  gradientSecundaryDisabled: 'rgba(223, 133, 29, 0.5)',
+
+  // bottom tab bar
+  tabbar: '#ffffff',
+  tabbarTint: '#555',
+  tabbarColor: '#aaa',
+
+  // Regular Colors
+  black: '#000',
+  yellow: '#ebbe3f',
+  green: '#3ac6b6',
+  orange: '#fa7e36',
+  danger: '#e43030',
+  red: '#da0909',
+  heart: '#fa8075',
+  cyan: '#03e2e3',
+  white: '#fff',
+
+  // Map
+  clusterBackground: '#fadec4',
+
+  // Label
+  success: '#16a74f',
+  warning: '#fff9b1',
+  warningLabel: '#caa100',
+};
